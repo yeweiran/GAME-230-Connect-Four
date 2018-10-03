b86c2beef1e7b3ef5f8d09b9e619b898d7a887dc
WEIRAN YE <wye12@ucsc.edu>
2018-10-03 03:49:08 -0700

    Bonus1、2Finish

--- GAME-230-Connect-Four/main.cpp
+++ GAME-230-Connect-Four/main.cpp
@@ -17,7 +17,11 @@ enum GAMEMODE
 GAMEMODE mode;
 
 int InitialBoard() {
-	//TODO
+	for (int i = 0; i < ROW; i++) {
+		for (int j = 0; j < COLUMN; j++) {
+			BOARD[i][j] = 0;
+		}
+	}
 	return 0;
 }
 
@@ -25,17 +29,19 @@ int ShowMainMenu() {
 	int modeNum;
 	
 	int modeFlag = 1;
+	system("cls");
 	cout << "This is a Connect Four game.\nPlease choose the game mode you want to play.\n"
 		<< "\t1.Simple mode\n"
 		<< "\t2.Customized Board\n"
 		<< "\t3.Wrap Around\n"
 		<< "\t4.Remove Enable\n"
 		<< "\t5.Play With A.I.\n"
+		<< "\t6.Exit\n"
 		<< ">";
 	
 	while (modeFlag) {
 		cin >> modeNum;
-		if (cin.fail() || modeNum > 5 || modeNum < 1) {
+		if (cin.fail() || modeNum > 6 || modeNum < 1) {
 			cout << "Invalid input, please try again.\n";
 			//fflush(stdin);
 			cin.clear();
@@ -44,26 +50,166 @@ int ShowMainMenu() {
 			cout << ">";
 		}
 		else {
-			//std::cout << modeNum;
+			if (modeNum == 6) {
+				return 1;
+			}
 			modeFlag = 0;
 			mode = (GAMEMODE)(modeNum - 1);
 			
 		}
 	}
-	
-	
-	system("cls");
 	return 0;
 }
 
 int CheckWin() {
-	//TODO
+	int temp = 0;
+	int tempI = 0;
+	int tempJ = 0;
+	int rest = 0;
+	for (int i = 0; i < ROW; i++) {
+		for (int j = 0; j < COLUMN; j++) {
+			if (BOARD[i][j] != 0) {
+				temp = BOARD[i][j];				
+				//Check ¡ú win
+				if ((j+REQTOWIN) <= COLUMN || mode == WRAPAROUND) {
+					tempI = i;
+					tempJ = j;
+					rest = REQTOWIN;
+					while (rest) {
+						tempJ++;
+						if (mode == WRAPAROUND) {
+							tempJ = tempJ % COLUMN;
+						}
+						if (BOARD[tempI][tempJ] != temp) {
+							break;
+						}
+						else
+						{
+							rest--;
+							if (rest == 1) {
+								return temp;
+							}
+						}						
+					}
+				}
+				//Check ¡ý win
+				if ((i+REQTOWIN) <= ROW) {
+					tempI = i;
+					tempJ = j;
+					rest = REQTOWIN;
+					while (rest) {
+						tempI++;
+						if (BOARD[tempI][tempJ] != temp) {
+							break;
+						}
+						else
+						{
+							rest--;
+							if (rest == 1) {
+								return temp;
+							}
+						}
+					}
+				}
+				//Check ¨K win
+				if (((j + REQTOWIN) <= COLUMN || mode == WRAPAROUND )&& (i + REQTOWIN) <= ROW) {
+					tempI = i;
+					tempJ = j;
+					rest = REQTOWIN;
+					while (rest) {
+						tempI++;
+						tempJ++;
+						if (mode == WRAPAROUND) {
+							tempJ = tempJ % COLUMN;
+						}
+						if (BOARD[tempI][tempJ] != temp) {
+							break;
+						}
+						else
+						{
+							rest--;
+							if (rest == 1) {
+								return temp;
+							}
+						}
+					}
+				}
+				//Check ¨L win
+				if (((j - REQTOWIN) >= -1 || mode == WRAPAROUND) && (i + REQTOWIN) <= ROW) {
+					tempI = i;
+					tempJ = j;
+					rest = REQTOWIN;
+					while (rest) {
+						tempI++;
+						tempJ--;
+						if (mode == WRAPAROUND) {
+							tempJ = (tempJ + COLUMN) % COLUMN;
+						}
+						if (BOARD[tempI][tempJ] != temp) {
+							break;
+						}
+						else
+						{
+							rest--;
+							if (rest == 1) {
+								return temp;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 	return 0;
 }
 
 int CheckDraw() {
-	//TODO
+	for (int i = 0; i < ROW; i++) {
+		for (int j = 0; j < COLUMN; j++) {
+			if (BOARD[i][j] == 0) {
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+int ShowBoard() {
+	for (int i = -1; i < ROW; i++) {
+		for (int j = 0; j < COLUMN; j++) {
+			if (i == -1) {
+				cout << j + 1;
+			}
+			else {
+				if (BOARD[i][j] == 0) {
+					cout << ".";
+				}
+				if (BOARD[i][j] == -1) {
+					cout << "X";
+				}
+				if (BOARD[i][j] == 1) {
+					cout << "O";
+				}
+			}	
+		}
+		cout << endl;
+	}
 	return 0;
+}
+
+//Return 0 means insert succeed, return 1 means the column is full
+int InsertPiece(int player, int column) {
+	int depth = ROW - 1;
+	while (depth >= 0) {
+		if (BOARD[depth][column - 1] == 0) {
+			BOARD[depth][column - 1] = player;
+			return 0;
+		}
+		else {
+			depth--;
+		}
+	}
+	return 1;
 }
 
 int GameFunc() {
@@ -73,20 +219,18 @@ int GameFunc() {
 	int reqToWinFlag = 1;
 	int turn = 0;
 	int gameEnd = 1;
+	int columnNum = 0;
+	int player = 0;
+	int winner = 0;
 	switch (mode)
 	{
-	case SIMPLE:
-		ROW = 6;
-		COLUMN = 7;
-		REQTOWIN = 4;
-		break;
-	case CUSTOMIZEDBOARD:		
+	case CUSTOMIZEDBOARD:
 		cout << "Please enter the number of rows.\n";
-		cout << ">";		
+		cout << ">";
 		while (rowFlag) {
 			cin >> ROW;
 			if (cin.fail() || ROW < 4 || ROW > 20) {
-				cout << "Invalid input, please try again.\n";				
+				cout << "Invalid input, please try again.\n";
 				cin.clear();
 				cin.ignore(1000, '\n');
 				cout << ">";
@@ -96,7 +240,7 @@ int GameFunc() {
 			}
 		}
 		cout << "Please enter the number of columns.\n";
-		cout << ">";		
+		cout << ">";
 		while (columnFlag) {
 			cin >> COLUMN;
 			if (cin.fail() || COLUMN < 4 || COLUMN > 20) {
@@ -113,7 +257,7 @@ int GameFunc() {
 		cout << ">";
 		while (reqToWinFlag) {
 			cin >> REQTOWIN;
-			if (cin.fail() || REQTOWIN < 3 || REQTOWIN > 20) {
+			if (cin.fail() || REQTOWIN < 3 || REQTOWIN > 20 || (REQTOWIN > ROW && REQTOWIN > COLUMN)) {
 				cout << "Invalid input, please try again.\n";
 				cin.clear();
 				cin.ignore(1000, '\n');
@@ -125,54 +269,96 @@ int GameFunc() {
 		}
 
 		break;
-	case WRAPAROUND:
-		ROW = 6;
-		COLUMN = 7;
-		REQTOWIN = 4;
-		break;
-	case REMOVE:
-		ROW = 6;
-		COLUMN = 7;
-		REQTOWIN = 4;
-		break;
-	case AI:
-		ROW = 6;
-		COLUMN = 7;
-		REQTOWIN = 4;
-		break;
 	default:
+		ROW = 6;
+		COLUMN = 7;
+		REQTOWIN = 4;
 		break;
 	}
-
-	//cout << "ROW:" << ROW << "\nCOLUMN:" << COLUMN << "\nRTW:" << REQTOWIN << endl;
 	while (gameEnd) {
-		if (CheckWin()) {
-			//TODO
+		system("cls");
+		ShowBoard();
+		winner = CheckWin();
+		if (winner == -1) {
+			cout << "Game Over!Player X has won!\n";
+			break;
+		}
+		else if (winner == 1) {
+			cout << "Game Over!Player O has won!\n";
+			break;
 		}
 		if (CheckDraw()) {
-
+			cout << "Game Over!This is a draw!\n";
+			break;
 		}
 
-
-
+		player = (turn % 2) * 2 - 1;
+		if (player == -1) {
+			cout << "This is the Player X`s turn\nPlease enter the column number to insert a piece.\n>";
+		}
+		if (player == 1) {
+			cout << "This is the Player O`s turn\nPlease enter the column number to insert a piece.\n>";
+		}
+		
+		while (true) {
+			cin >> columnNum;
+			if (cin.fail() || columnNum < 1 || columnNum > COLUMN) {
+				cout << "Invalid input, please try again.\n";
+				cin.clear();
+				cin.ignore(1000, '\n');
+				cout << ">";
+			}
+			else {
+				if (InsertPiece(player, columnNum)) {
+					cout << "This column is full, please try again.\n";
+					cin.clear();
+					cin.ignore(1000, '\n');
+					cout << ">";
+				}
+				else {
+					turn++;
+					break;
+				}
+				
+			}
+		}
 	}
+
 	return 0;
 }
 
 int GameControl() {
-
-	ShowMainMenu();
-	GameFunc();
-
+	char input;
+	while (true) {
+		if (ShowMainMenu()) {
+			return 0;
+		}
+		InitialBoard();
+		GameFunc();
+		cout << "Want another Game?(y/n)\n>";		
+		while (true) {
+			cin >> input;
+			if (cin.fail() || (input != 'y' && input != 'Y' && input != 'n' && input != 'N')) {
+				cout << "Invalid input, please try again.\n";
+				cin.clear();
+				cin.ignore(1000, '\n');
+				cout << ">";
+			}
+			else if (input == 'y' || input == 'Y') {
+				break;
+			}
+			else if (input == 'n' || input == 'N') {
+				return 0;
+			}
+		}
+	}	
 	return 0;
 }
 
 int main() {
-	
 	GameControl();
-	//ShowMainMenu();
-
-	//InitialBoard();
-	//cout << mode;
+	system("cls");
+	cout << "This is a simple game produced by Weiran Ye in UCSC GPM project GAME-230.\n"
+		<<"Thank you so much for playing my game!\n";
 	system("pause");
 }
